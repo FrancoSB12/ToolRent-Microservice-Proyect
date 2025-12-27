abbc1c5b5eb5c103db10ae0d96f8cb02825eda5b
Franco Saravia <franco.saravia@usach.cl>
2025-12-27 19:59:50 -0300

    Kardex microservice recreation

---
+++ kardex-service/src/test/java/com/toolrent/kardexservice/KardexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolrent.kardexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
