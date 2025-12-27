dfabdcb2d17507c005d9b3f76dc60cd73de72f0c
Franco Saravia <franco.saravia@usach.cl>
2025-12-27 19:43:09 -0300

    Project recreation

--- client-service/src/main/java/com/toolrent/clientservice/ClientserviceApplication.java
+++ client-service/src/main/java/com/toolrent/clientservice/ClientserviceApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class ClientserviceApplication {
+public class ClientServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientserviceApplication.class, args);
+		SpringApplication.run(ClientServiceApplication.class, args);
 	}
 
 }
