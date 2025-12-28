417ae2801abc07291f9185d38c95cd008d095764
Franco Saravia <franco.saravia@usach.cl>
2025-12-28 18:11:10 -0300

    Inventory microservice entities and enums created

---
+++ inventory-service/src/main/java/com/toolrent/inventoryservice/Enum/ToolDamageLevel.java
@@ -0,0 +1,11 @@
+package com.toolrent.inventoryservice.Enum;
+
+public enum ToolDamageLevel {
+    NO_DANADA,
+    LEVEMENTE_DANADA,
+    DANADA,
+    GRAVEMENTE_DANADA,
+    IRREPARABLE,
+    DESUSO,
+    EN_EVALUACION
+}
