8362db4fde6aaa25a296251c815e0c7864fab2e0
Franco Saravia <franco.saravia@usach.cl>
2025-12-29 21:10:12 -0300

    Encapsulation of parts of createLoan() for better development

--- rent-service/src/main/java/com/toolrent/rentservice/Service/RentService.java
+++ rent-service/src/main/java/com/toolrent/rentservice/Service/RentService.java
@@ -38,38 +38,10 @@ public class RentService {
     @Transactional
     public RentEntity createLoan(RentEntity rent, String employeeRun) {
         //The employee is searched in the database
-        Employee employee;
-        try {
-            employee = getEmployee(employeeRun);
-        } catch (HttpClientErrorException.NotFound e) {
-            //The employee microservice responded, but said that the employee doesn't exist
-            throw new RuntimeException("Empleado no encontrado con RUN: " + employeeRun);
-
-        } catch (HttpClientErrorException | HttpServerErrorException e) {
-            //The employee microservice responded, but with another error (400, 401, 500, etc.)
-            throw new RuntimeException("Error en servicio de empleados: " + e.getResponseBodyAsString());
-
-        } catch (RestClientException e) {
-            //The employee microservice didn't respond (Off, Timeout, DNS, etc.)
-            throw new RuntimeException("El servicio de empleados está caído o no responde.");
-        }
+        Employee employee = fetchEmployeeInDB(employeeRun);
 
         //The client is searched in the database
-        Client client;
-        try {
-            client = getClient(rent.getClientRun());
-        } catch (HttpClientErrorException.NotFound e) {
-            //The client microservice responded, but said that the client doesn't exist
-            throw new RuntimeException("Cliente no encontrado con RUN: " + rent.getClientRun());
-
-        } catch (HttpClientErrorException | HttpServerErrorException e) {
-            //The client microservice responded, but with another error (400, 401, 500, etc.)
-            throw new RuntimeException("Error en servicio de clientes: " + e.getResponseBodyAsString());
-
-        } catch (RestClientException e) {
-            //The client microservice didn't respond (Off, Timeout, DNS, etc.)
-            throw new RuntimeException("El servicio de clientes está caído o no responde.");
-        }
+        Client client = fetchClientInDB(employeeRun);
 
         //Check that the client doesn't have a debt
         if (client.getStatus().equals("Restringido") || client.getDebt() > 0) {
@@ -99,40 +71,11 @@ public class RentService {
 
         for (RentXToolItemEntity toolItemRequest : toolItemsFromFront) {
             Long toolItemId = toolItemRequest.getToolItemId();
-            Long toolTypeId = toolItemRequest.getToolTypeId();
-
-            ToolItem toolItem;
-            try {
-                toolItem = getToolItem(toolItemId);
-            } catch (HttpClientErrorException.NotFound e) {
-                //The inventory microservice responded, but said that the tool item doesn't exist
-                throw new RuntimeException("La herramienta con ID " + toolItemId + " no existe.");
-
-            } catch (HttpClientErrorException | HttpServerErrorException e) {
-                //The inventory microservice responded, but with another error (400, 401, 500, etc.)
-                throw new RuntimeException("Error en servicio de inventario: " + e.getResponseBodyAsString());
-
-            } catch (RestClientException e) {
-                //The inventory microservice didn't respond (Off, Timeout, DNS, etc.)
-                throw new RuntimeException("El servicio de inventario está caído o no responde.");
-            }
+            ToolItem toolItem = fetchToolItemInDB(toolItemId);
 
             //The tool type from the tool item is saved
-            ToolType toolType;
-            try {
-                toolType = getToolType(toolTypeId);
-            } catch (HttpClientErrorException.NotFound e) {
-                //The inventory microservice responded, but said that the tool item doesn't exist
-                throw new RuntimeException("El tipo de herramienta con ID " + toolTypeId + " no existe.");
-
-            } catch (HttpClientErrorException | HttpServerErrorException e) {
-                //The inventory microservice responded, but with another error (400, 401, 500, etc.)
-                throw new RuntimeException("Error en servicio de inventario: " + e.getResponseBodyAsString());
-
-            } catch (RestClientException e) {
-                //The inventory microservice didn't respond (Off, Timeout, DNS, etc.)
-                throw new RuntimeException("El servicio de inventario está caído o no responde.");
-            }
+            Long toolTypeId = toolItemRequest.getToolTypeId();
+            ToolType toolType = fetchToolTypeInDB(toolTypeId);
 
             //The stock and status are validated
             if (!toolItem.getStatus().equals("DISPONIBLE") || toolType.getAvailableStock() < 1) {
@@ -172,79 +115,145 @@ public class RentService {
         RentEntity savedLoan = rentRepository.save(rent);
 
         //Create and save the associated kardex for each tool, reduce the tool stock and save the relationships
-        // A. Actualizar estado de herramientas a "PRESTADA"
         for (RentXToolItemEntity item : toolItemsToSave) {
             //Link to the saved loan
             item.setRent(savedLoan);
             rentXToolItemService.save(item);
 
-            try {
-                //Update status
-                updateStatus(item);
-            } catch (RestClientException e) {
-                throw new RuntimeException("Error actualizando estado en inventario. Datos inconsistentes.");
-            }
+            //Update status
+            updateStatus(item);
 
-            try {
-                //Update Stock
-                updateStock(item.getToolTypeId());
-            } catch (RestClientException e) {
-                throw new RuntimeException("Error actualizando stock en inventario. Datos inconsistentes.");
-            }
+            //Update Stock
+            updateStock(item.getToolTypeId());
 
-            try {
-                //Create associated kardex
-                createKardex(item.getToolTypeId(), "PRESTAMO");
-            } catch (RestClientException e) {
-                throw new RuntimeException("Error creando kardex. Datos inconsistentes.");
-            }
+            //Create associated kardex
+            createKardex(item.getToolTypeId(), "PRESTAMO");
         }
 
         //Update the client active rents
-        try {
-            updateActiveRents(client);
-        } catch (RestClientException e) {
-            throw new RuntimeException("Error actualizando datos del cliente.");
-        }
+        updateActiveRents(client);
 
         return savedLoan;
     }
 
     //Private methods
-    private Employee getEmployee(String run) {
-        return restTemplate.getForObject("http://employee-service/employee/" + run, Employee.class);
-    }
-
-    private Client getClient(String run) {
-        return restTemplate.getForObject("http://client-service/client/" + run, Client.class);
-    }
-
     private ToolItem getToolItem(Long id) {
         return restTemplate.getForObject("http://inventory-service/inventory/tool-item/" + id, ToolItem.class);
     }
 
-    private ToolType getToolType(Long id) {
-        return restTemplate.getForObject("http://inventory-service/inventory/tool-type/" + id, ToolType.class);
-    }
-
     private void updateStatus(RentXToolItemEntity item){
-        ToolItem toolItem = getToolItem(item.getToolItemId());
-        toolItem.setStatus("PRESTADA");
-        restTemplate.put("http://inventory-service/inventory/tool-item/" + toolItem.getId(), toolItem, ToolItem.class);
+        try {
+            ToolItem toolItem = getToolItem(item.getToolItemId());
+            toolItem.setStatus("PRESTADA");
+            restTemplate.put("http://inventory-service/inventory/tool-item/" + toolItem.getId(), toolItem, ToolItem.class);
+        } catch (RestClientException e) {
+            throw new RuntimeException("Error actualizando estado en inventario. Datos inconsistentes.");
+        }
     }
 
     private void updateStock(Long toolTypeId) {
-        ChangeStockRequest changeStockRequest = new ChangeStockRequest(toolTypeId, -1);
-        restTemplate.put("http://inventory-service/inventory/tool-type/change-available-stock", changeStockRequest, ToolType.class);
+        try {
+            ChangeStockRequest changeStockRequest = new ChangeStockRequest(toolTypeId, -1);
+            restTemplate.put("http://inventory-service/inventory/tool-type/change-available-stock", changeStockRequest, ToolType.class);
+        } catch (RestClientException e) {
+            throw new RuntimeException("Error actualizando stock en inventario. Datos inconsistentes.");
+        }
     }
 
     private void createKardex(Long toolTypeId, String operationType) {
-        CreateKardexRequest createKardexRequest = new CreateKardexRequest(toolTypeId, operationType, 1);
-        restTemplate.postForObject("http://kardex-service/kardex/entry", createKardexRequest, Void.class);
+        try {
+            CreateKardexRequest createKardexRequest = new CreateKardexRequest(toolTypeId, operationType, 1);
+            restTemplate.postForObject("http://kardex-service/kardex/entry", createKardexRequest, Void.class);
+        } catch (RestClientException e) {
+            throw new RuntimeException("Error creando kardex. Datos inconsistentes.");
+        }
     }
 
     private void updateActiveRents(Client client) {
-        client.setActiveRents(client.getActiveRents() + 1);
-        restTemplate.put("http://client-service/client/" + client.getRun(), Client.class);
+        try {
+            client.setActiveRents(client.getActiveRents() + 1);
+            restTemplate.put("http://client-service/client/" + client.getRun(), Client.class);
+        } catch (RestClientException e) {
+            throw new RuntimeException("Error actualizando datos del cliente.");
+        }
+    }
+
+    private Employee fetchEmployeeInDB(String run) {
+        Employee employee;
+        try {
+            employee = restTemplate.getForObject("http://employee-service/employee/" + run, Employee.class);
+        } catch (HttpClientErrorException.NotFound e) {
+            //The employee microservice responded, but said that the employee doesn't exist
+            throw new RuntimeException("Empleado no encontrado con RUN: " + run);
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            //The employee microservice responded, but with another error (400, 401, 500, etc.)
+            throw new RuntimeException("Error en servicio de empleados: " + e.getResponseBodyAsString());
+
+        } catch (RestClientException e) {
+            //The employee microservice didn't respond (Off, Timeout, DNS, etc.)
+            throw new RuntimeException("El servicio de empleados está caído o no responde.");
+        }
+        return employee;
+    }
+
+    private Client fetchClientInDB(String run) {
+        Client client;
+        try {
+            client = restTemplate.getForObject("http://client-service/client/" + run, Client.class);
+        } catch (HttpClientErrorException.NotFound e) {
+            //The client microservice responded, but said that the client doesn't exist
+            throw new RuntimeException("Cliente no encontrado con RUN: " + run);
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            //The client microservice responded, but with another error (400, 401, 500, etc.)
+            throw new RuntimeException("Error en servicio de clientes: " + e.getResponseBodyAsString());
+
+        } catch (RestClientException e) {
+            //The client microservice didn't respond (Off, Timeout, DNS, etc.)
+            throw new RuntimeException("El servicio de clientes está caído o no responde.");
+        }
+
+        return client;
+    }
+
+    private ToolItem fetchToolItemInDB(Long id) {
+        ToolItem toolItem;
+        try {
+            toolItem = getToolItem(id);
+        } catch (HttpClientErrorException.NotFound e) {
+            //The inventory microservice responded, but said that the tool item doesn't exist
+            throw new RuntimeException("La herramienta con ID " + id + " no existe.");
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            //The inventory microservice responded, but with another error (400, 401, 500, etc.)
+            throw new RuntimeException("Error en servicio de inventario: " + e.getResponseBodyAsString());
+
+        } catch (RestClientException e) {
+            //The inventory microservice didn't respond (Off, Timeout, DNS, etc.)
+            throw new RuntimeException("El servicio de inventario está caído o no responde.");
+        }
+
+        return toolItem;
+    }
+
+    private ToolType fetchToolTypeInDB(Long id) {
+        ToolType toolType;
+        try {
+            toolType = restTemplate.getForObject("http://inventory-service/inventory/tool-type/" + id, ToolType.class);
+        } catch (HttpClientErrorException.NotFound e) {
+            //The inventory microservice responded, but said that the tool item doesn't exist
+            throw new RuntimeException("El tipo de herramienta con ID " + id + " no existe.");
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            //The inventory microservice responded, but with another error (400, 401, 500, etc.)
+            throw new RuntimeException("Error en servicio de inventario: " + e.getResponseBodyAsString());
+
+        } catch (RestClientException e) {
+            //The inventory microservice didn't respond (Off, Timeout, DNS, etc.)
+            throw new RuntimeException("El servicio de inventario está caído o no responde.");
+        }
+
+        return toolType;
     }
 }
