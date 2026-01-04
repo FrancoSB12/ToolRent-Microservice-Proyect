42da7a884a4732a05efc243874e6840681b03745
Franco Saravia <franco.saravia@usach.cl>
2026-01-04 19:05:06 -0300

    Docker configuration

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE "ToolRent-client";
+CREATE DATABASE "ToolRent-employee";
+CREATE DATABASE "ToolRent-fee";
+CREATE DATABASE "ToolRent-inventory";
+CREATE DATABASE "ToolRent-kardex";
+CREATE DATABASE "ToolRent-rent";
+CREATE DATABASE "ToolRent-report";
+CREATE DATABASE "keycloak_db";
